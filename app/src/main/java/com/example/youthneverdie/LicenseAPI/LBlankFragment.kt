cac33991667049872d77f63c7b0f45ebd6f74650
서영 <hongsy7179@gmail.com>
2023-12-06 09:36:59 +0900

    1.0.0-YouthNeverDie

--- app/src/main/java/com/example/youthneverdie/LicenseAPI/LBlankFragment.kt
+++ app/src/main/java/com/example/youthneverdie/LicenseAPI/LBlankFragment.kt
@@ -27,7 +27,6 @@ class LBlankFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_l_blank, container, false)
     }
 
